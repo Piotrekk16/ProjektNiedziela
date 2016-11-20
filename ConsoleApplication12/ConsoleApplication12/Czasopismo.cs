cab6b8ac367c0b5db4c79c9d06042d2be9d0fd22
Piotrekk85 <piotr.jastr@gmail.com>
2016-11-20 08:31:40 +0100

    Add files via upload

---
+++ ConsoleApplication12/ConsoleApplication12/Czasopismo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Czasopismo :Publikacja
+    {
+        private int nr;
+
+        public void WypiszInfo()
+        {
+            Console.WriteLine("Tytuł {0}",tytul);
+            Console.WriteLine("Numer {0}", nr);
+        }
+
+        public Czasopismo() { nr = 1; }
+
+        public Czasopismo(string tytul,int nr)
+        {
+            this.tytul = tytul;
+            this.nr = nr;
+        }
+    }
+}
---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ksiazka book1 = new Ksiazka();
+            book1.UstawAutoraITytul("Henryk Sienkiewicz", "Krzyżacy");
+            book1.UstawIloscStron(200);
+            Console.WriteLine(book1.PodajCene());
+
+            Czasopismo cz1 = new Czasopismo("Wyborcza", 234);
+            cz1.WypiszInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
